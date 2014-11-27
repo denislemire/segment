1ce04fd901caf4747a9a59ae2e63277c221211b6
Denis Lemire <denis.lemire@thinktel.ca>
2014-11-26 23:37:54 -0700

    Remove all the needless functions.

--- segment.ino
+++ segment.ino
@@ -1,17 +1,3 @@
-/*
-Make Projects: How to Drive a 7 Segment LED
- URL: http://makezine.com/projects/drive-a-7-segment-led-with-an-arduino/
- By: Riley Porter
- This is an introduction on how to drive a 7 Segment LED using only a Arduino.  This is 
- not the best way to do this.  This is meant to be a learning excercise.  In later tutorials
- I will show you how to use an dedicated IC using SPI or a Shift Register.  Enjoy.
- 
- 
- digitalWrite(A, LOW) = turn off the "A" segment in the LED display
- digitalWrite(B, HIGH)  = turn on the "B" segment in the LED display
- 
- */
-
 const int A=8;
 const int B=9;
 const int C=2;
@@ -20,264 +6,60 @@ const int E=4;
 const int F=5;
 const int G=6;
 
-void clr()
+void setSegment (unsigned int value)
 {
-  //Clears the LED
-  digitalWrite(A, LOW);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-  digitalWrite(D, LOW);
-  digitalWrite(E, LOW);
-  digitalWrite(F, LOW);
-  digitalWrite(G, LOW);
-
+	digitalWrite(A, value & (1<<6));
+	digitalWrite(B, value & (1<<5));
+	digitalWrite(C, value & (1<<4));
+	digitalWrite(D, value & (1<<3));
+	digitalWrite(E, value & (1<<2));
+	digitalWrite(F, value & (1<<1));
+	digitalWrite(G, value & (1<<0));
 }
 
-
-void char_A()
+void loop()
 {
-  digitalWrite(D, LOW);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
+	setSegment (0b1110111);
+  	delay(1000);
+	setSegment (0b0011111);
+	delay(1000);
+	setSegment (0b0001110);
+	delay(1000);
+	setSegment (0b0111110);
+	delay(1000);
+	setSegment (0b1001111);
+	delay(1000);
+	setSegment (0b1000111);
+	delay(1000);
+	setSegment (0b0110000);
+	delay(1000);
+	setSegment (0b1101110);
+	delay(1000);
+	setSegment (0b1111010);
+	delay(1000);
+	setSegment (0b0110011);
+	delay(1000);
+	setSegment (0b1011011);
+	delay(1000);
+	setSegment (0b1011111);
+	delay(1000);
+	setSegment (0b1110000);
+	delay(1000);
+	setSegment (0b1111111);
+	delay(1000);
+	setSegment (0b1111011);
+	delay(1000);
+	setSegment (0b1111101);
+	delay(1000);
 }
 
-void char_B()
+void setup()
 {
-  //Displays B
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, LOW);
-  digitalWrite(B, LOW);
-  digitalWrite(C, HIGH);
-}
-
-void char_C()
-{
-  //Displays C
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, LOW);
-  digitalWrite(A, LOW);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-}
-
-void char_D()
-{
-  //Displays D
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, LOW);
-  digitalWrite(A, LOW);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-}
-
-void char_E()
-{
-  //Displays E
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-}
-
-void char_F()
-{
-  //Displays F
-  digitalWrite(D, LOW);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-}
-
-
-void one()
-{
-  //Displays 1
-  digitalWrite(D, LOW);
-  digitalWrite(E, LOW);
-  digitalWrite(F, LOW);
-  digitalWrite(G, LOW);
-  digitalWrite(A, LOW);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-}
-
-void two()
-{
-  //Displays 2
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, LOW);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, LOW);
-}
-
-void three()
-{
-  //Displays 3
-  digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, LOW);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-}
-
-void four()
-{
-  //Displays 4
-  digitalWrite(D, LOW);
-  digitalWrite(E, LOW);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, LOW);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-}
-
-void five()
-{
-  //Displays 5
-  digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(C, HIGH);
-}
-
-void six()
-{
-  //Displays 6
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(C, HIGH);
-}
-
-void seven()
-{
-  //Displays 7
-  digitalWrite(D, LOW);
-  digitalWrite(E, LOW);
-  digitalWrite(F, LOW);
-  digitalWrite(G, LOW);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-}
-
-void eight()
-{
-  //Displays 8
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-}
-
-void nine()
-{
-  //Displays 9
-  digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-}
-
-void zero()
-{
-  //Displays 0
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, LOW);
-  digitalWrite(G, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-}
-
-void LoopDisplay()
-{
-  //Loop through all Chars and Numbers
-  char_A();
-  delay(1000);
-  char_B();
-  delay(1000);
-  char_C();
-  delay(1000);
-  char_D();
-  delay(1000);
-  char_E();
-  delay(1000);
-  char_F();
-  delay(1000);
-  one();
-  delay(1000);
-  two();
-  delay(1000);
-  three();
-  delay(1000);
-  four();
-  delay(1000);
-  five();
-  delay(1000);
-  six();
-  delay(1000);
-  seven();
-  delay(1000);
-  eight();
-  delay(1000);
-  nine();
-  delay(1000);
-  zero();
-  delay(1000);
-}
-
-void setup() {
-  // put your setup code here, to run once:
-  //Setup our pins
-  pinMode(A, OUTPUT);
-  pinMode(B, OUTPUT);
-  pinMode(C, OUTPUT);
-  pinMode(D, OUTPUT);
-  pinMode(E, OUTPUT);
-  pinMode(F, OUTPUT);
-  pinMode(G, OUTPUT);
-}
-
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  LoopDisplay();
-  // 2014-11-08
+	pinMode(A, OUTPUT);
+	pinMode(B, OUTPUT);
+	pinMode(C, OUTPUT);
+	pinMode(D, OUTPUT);
+	pinMode(E, OUTPUT);
+	pinMode(F, OUTPUT);
+	pinMode(G, OUTPUT);
 }
